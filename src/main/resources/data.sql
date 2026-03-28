7fcdb9af85f2488256bf723912a4b8a6424c073e
conradrenno <conradrenno@hotmail.com>
2026-03-28 12:49:17 -0300

    Containerized application

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,13 @@
 
-INSERT INTO usuario(nome, email, login, senha, data_ultima_alteracao,rua,bairro,cidade,estado,cep)
+/*INSERT INTO usuario(nome, email, login, senha, data_ultima_alteracao,rua,bairro,cidade,estado,cep)
 VALUES ('Marcelo', 'marcelo@gmail.com', 'marcelo12', '$2a$12$T3uoV3k6XC.Sz1IivTKLQuzzEuGpqoR4Kobk.X83f2/H9HQ0DzlQi', '2023-01-01', 'Rua 1', 'Bairro 1', 'Cidade 1', 'Estado 1', '12345678');
 
 INSERT INTO usuario(nome, email, login, senha, data_ultima_alteracao,rua,bairro,cidade,estado,cep)
 VALUES ('Amy', 'amy@gmail.com', 'amy12', '$2a$12$T3uoV3k6XC.Sz1IivTKLQuzzEuGpqoR4Kobk.X83f2/H9HQ0DzlQi', '2024-01-01', 'Rua 2', 'Bairro 2', 'Cidade 2', 'Estado 2', '12345679');
+*/
 
+INSERT INTO usuario (nome, email, login, senha, data_ultima_alteracao, rua, bairro, cidade, estado, cep)
+VALUES
+    ('Marcelo', 'marcelo@gmail.com', 'marcelo12', '$2a$12$T3uoV3k6XC.Sz1IivTKLQuzzEuGpqoR4Kobk.X83f2/H9HQ0DzlQi', '2023-01-01 00:00:00', 'Rua 1', 'Bairro 1', 'Cidade 1', 'Estado 1', '12345678'),
 
+    ('Amy', 'amy@gmail.com', 'amy12', '$2a$12$T3uoV3k6XC.Sz1IivTKLQuzzEuGpqoR4Kobk.X83f2/H9HQ0DzlQi', '2024-01-01 00:00:00', 'Rua 2', 'Bairro 2', 'Cidade 2', 'Estado 2', '12345679');
